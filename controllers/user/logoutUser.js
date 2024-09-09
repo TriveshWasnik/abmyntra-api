e51cf118eadaf26a9f84bdc1860ea0b32e8eb043
TriveshWasnik <triveshwasnik88@gmail.com>
2024-09-09 23:30:43 +0530

    update

--- controllers/user/logoutUser.js
+++ controllers/user/logoutUser.js
@@ -3,7 +3,11 @@ export const logoutUser = async function (req, res) {
   try {
     return res
       .status(200)
-      .clearCookie("token", { httpOnly: true, secure: true, sameSite: true })
+      .clearCookie("token", {
+        httpOnly: true,
+        secure: true,
+        sameSite: "strict",
+      })
       .json({ message: "User logged out successfully", success: true });
   } catch (error) {
     console.log(error);

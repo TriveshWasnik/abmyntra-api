48ad17bd320c8eab4f3934f149a762a3fd2aade7
TriveshWasnik <triveshwasnik88@gmail.com>
2024-09-16 21:49:15 +0530

    update

--- routes/wishlist.route.js
+++ routes/wishlist.route.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { addProductWishlist } from "../controllers/wishlist/addProduct.js";
+
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import { deleteProductFromWishlist } from "../controllers/wishlist/deleteProduct.js";
 import { listWishProduct } from "../controllers/wishlist/listWishlist.js";
+import { addProductWishlist } from "../controllers/wishlist/addProductWishlist.js";
 const router = Router();
 
-router.route("/addProduct/:id").put(verifyJWT, addProductWishlist);
+router.route("/addwishlist/:id").put(verifyJWT, addProductWishlist);
 router.route("/deleteProduct/:id").put(verifyJWT, deleteProductFromWishlist);
 router.route("/list").get(verifyJWT, listWishProduct);
 export default router;

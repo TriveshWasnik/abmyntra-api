d592451ecf388adec603aaa2818b61dafe8b0555
TriveshWasnik <triveshwasnik88@gmail.com>
2024-09-20 15:24:38 +0530

    update

--- controllers/wishlist/listWishlist.js
+++ controllers/wishlist/listWishlist.js
@@ -4,9 +4,9 @@ import { Wishlist } from "../../models/wishlist.model.js";
 export const listWishProduct = async function (req, res) {
   try {
     const products = await Wishlist.find({})
-      .populate("userId", "name email phoneNumber")
+      .populate("user", "name email phoneNumber")
       .populate(
-        "productId",
+        "product",
         "name productImage1 description weight weightType mrpPrice"
       );
     return res.status(200).json({
@@ -17,7 +17,7 @@ export const listWishProduct = async function (req, res) {
   } catch (error) {
     console.log(error);
     res.status(500).json({
-      message: "Error in get Wishlist List APi",
+      message: "Error in get Wishlist List API",
       success: false,
       error,
     });

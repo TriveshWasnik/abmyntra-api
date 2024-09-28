710e4b90b80a766fcc9fcc7011c2042b1fb33f28
TriveshWasnik <92360264+TriveshWasnik@users.noreply.github.com>
2024-09-29 01:10:58 +0530

    Update

--- controllers/cart/cartListUser.js
+++ controllers/cart/cartListUser.js
@@ -1,54 +1,15 @@
-import { Cart } from "../../models/cart.model.js";
+import { User } from "../../models/user.model.js";
 
 // cart list user
 export const cartListUser = async function (req, res) {
   try {
-    const userId = req.id;
-
-    const userCart = await Cart.find({
-      userId,
-      orderStatus: "add to cart",
-    })
-      .populate(
-        "productId",
-        "name productImage1 weight weightType mrpPrice sellingPrice"
-      )
-      .populate("userId", "name email phoneNumber");
-
-    if (!userCart) {
-      return res
-        .status(400)
-        .json({ message: "User Cart is Empty", success: false });
-    }
-
-    // calculate total amount and total item number
-    let totalAmountWithDiscount = 0;
-    let totalAmountWithoutDiscount = 0;
-    let totalItems = 0;
-    let totalDiscount = 0;
-
-    userCart.forEach((item) => {
-      totalAmountWithDiscount +=
-        item.productQuantity * item.productId.sellingPrice;
-      totalAmountWithoutDiscount +=
-        item.productQuantity * item.productId.mrpPrice;
-      totalItems = item.productQuantity;
-      const discount = item.productId.mrpPrice - item.productId.sellingPrice;
-      totalDiscount = discount * item.productQuantity;
+   const userJWT = req.user;
+    const user = await User.findById(userJWT.id).populate({
+      path: "cart.product",
+      model: "Product",
     });
-
-    const shippingCharges = calculate15Percent(totalAmountWithDiscount);
-
-    return res.status(200).json({
-      data: userCart,
-      totalAmountWithDiscountSubtotal: totalAmountWithDiscount,
-      totalAmountWithDiscount: totalAmountWithDiscount * shippingCharges,
-      totalAmountWithoutDiscount,
-      totalItems,
-      totalDiscount,
-      shippingCharges,
-      success: true,
-    });
+    const cartItems = user.cart;
+    return res.status(200).json(cartItems);
   } catch (error) {
     console.log(error.message);
     res
@@ -57,6 +18,3 @@ export const cartListUser = async function (req, res) {
   }
 };
 
-function calculate15Percent(totalAmount) {
-  return totalAmount * 0.15;
-}

4976c9014874e812ce0b66e3fb980c9219e27ec0
TriveshWasnik <92360264+TriveshWasnik@users.noreply.github.com>
2024-09-29 01:52:27 +0530

    Update

--- routes/order.route.js
+++ routes/order.route.js
@@ -1,13 +1,11 @@
 import { Router } from "express";
 import { createOrder } from "../controllers/order/createOrder.js";
-import { isAdmin, verifyJWT } from "../middlewares/auth.middleware.js";
-import { orderListAdmin } from "../controllers/order/orderListAdmin..js";
+import {  verifyJWT } from "../middlewares/auth.middleware.js";
+
 import { orderListUser } from "../controllers/order/orderListUser.js";
-import { singleOrder } from "../controllers/order/singleOrder.js";
+
 const router = Router();
 
 router.route("/create").post(verifyJWT, createOrder);
-router.route("/orderlistadmin").get(verifyJWT, isAdmin, orderListAdmin);
-router.route("/orderlistuser").get(verifyJWT, isAdmin, orderListUser);
-router.route("/:id").get(verifyJWT, singleOrder);
+router.route("/orderlistuser").get(verifyJWT, orderListUser);
 export default router;

e3209b60a8efc36299df9d689545b84218838dfa
TriveshWasnik <92360264+TriveshWasnik@users.noreply.github.com>
2024-09-29 00:54:36 +0530

    Update

--- models/order.model.js
+++ models/order.model.js
@@ -2,42 +2,31 @@ import mongoose from "mongoose";
 
 const orderSchema = new mongoose.Schema(
   {
-    orderId: { type: String, required: true },
-    shippingName: { type: String, required: true },
-    shippingAddress1: { type: String, required: true },
-    shippingAddress2: { type: String, required: true },
-    shippingCountry: { type: String, required: true },
-    shippingState: { type: String, required: true },
-    shippingCity: { type: String, required: true },
-    shippingPincode: { type: String, required: true },
-    shippingEmail: { type: String, required: [true, "Email is required"] },
-    shippingPhone: { type: String, required: [true, "Mobile is required"] },
-    couponName: { type: String, default: null },
-    couponAmount: { type: Number, default: 0 },
-    paymentKey: { type: String, default: null },
-    orderDate: { type: Date, default: Date.now },
-    orderStatus: {
-      type: String,
-      enum: ["pending", "processing", "shipped", "delivered"],
-      default: "pending",
+    products: {
+      type: [
+        {
+          product: { type: mongoose.Schema.Types.ObjectId, ref: "Product" },
+          quantity: { type: Number, default: 1 },
+        },
+      ],
+      required: true,
     },
-    paymentStatus: {
-      type: String,
-      enum: ["pending", "received"],
-      default: "pending",
-    },
-    paymentMethod: {
-      type: String,
-      enum: ["COD", "Online"],
-      default: "COD",
-    },
-    shippingCharges: { type: Number, default: 0 },
-    taxAmount: { type: Number, default: 0 },
-    subTotalAmount: { type: Number },
-    grandTotalAmount: { type: Number },
-    userId: {
+    user: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
+      required: true,
+    },
+    total_amount: {
+      type: mongoose.Types.Decimal128,
+      required: true,
+    },
+    address: {
+      type: String,
+      default: "",
+    },
+    status: {
+      type: String,
+      default: "Payment Done",
     },
   },
   { timestamps: true }

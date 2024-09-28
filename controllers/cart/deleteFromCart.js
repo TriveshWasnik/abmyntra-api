6b2ccb7cd0bfe4e3a8bcf0b4bdce8bb8ec4ebbcd
TriveshWasnik <92360264+TriveshWasnik@users.noreply.github.com>
2024-09-29 01:09:04 +0530

    Update

--- controllers/cart/deleteFromCart.js
+++ controllers/cart/deleteFromCart.js
@@ -1,16 +1,26 @@
-import { Cart } from "../../models/cart.model.js";
+import { User } from "../../models/user.model.js";
 
 // delete product From Cart
 export const deleteFromCart = async function (req, res) {
   try {
-    const cartItem = await Cart.findByIdAndDelete(req.params.id);
-
-    if (!cartItem) {
-      return res
-        .status(400)
-        .json({ message: "Cart Item not found", success: false });
-    }
-
+    
+    const { productId, quantity } = req.body;
+    const userJWT = req.user;
+    const user = await User.findById(userJWT.id);
+   
+    const productIndex = user.cart.findIndex((item) =>
+      item.product.equals(productId)
+    );
+    if (productIndex !== -1) {
+      if (quantity && quantity > 0) {
+        user.cart[productIndex].quantity -= quantity;
+        if (user.cart[productIndex].quantity <= 0) {
+          user.cart.splice(productIndex, 1);
+        }
+      } else {
+        user.cart.splice(productIndex, 1);
+      }
+      await user.save();
     return res.status(200).json({
       message: `Cart items deleted successfully `,
       success: true,

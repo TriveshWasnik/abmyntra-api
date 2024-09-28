cf166314f796f41af84f1970abccb7a2e116c386
TriveshWasnik <92360264+TriveshWasnik@users.noreply.github.com>
2024-09-29 01:46:18 +0530

    Update

--- controllers/order/orderListUser.js
+++ controllers/order/orderListUser.js
@@ -6,13 +6,13 @@ export const orderListUser = async function (req, res) {
   try {
    
    const user = req.user;
-    const orders = await Orders.find({ user: user.id });
+    const orders = await Order.find({ user: user.id });
     return res.status(200).json(orders);
    
   } catch (error) {
     console.log(error.message);
     res
       .status(400)
-      .json({ message: "Error in Create a Add to Cart API", success: false });
+      .json({ message: "Error in Order list API", success: false });
   }
 };

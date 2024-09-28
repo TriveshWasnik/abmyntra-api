adab46e5b11b1add2b6f861903634e205c2f2d79
TriveshWasnik <92360264+TriveshWasnik@users.noreply.github.com>
2024-09-29 00:56:27 +0530

    Update

--- models/wishlist.model.js
+++ models/wishlist.model.js
@@ -1,19 +1,1 @@
-import mongoose from "mongoose";
 
-const wishlistSchema = new mongoose.Schema(
-  {
-    productName: { type: String },
-    product: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Product",
-      default: null,
-    },
-    user: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-    },
-  },
-  { timestamps: true }
-);
-
-export const Wishlist = mongoose.model("Wishlist", wishlistSchema);

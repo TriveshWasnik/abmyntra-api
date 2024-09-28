e5d6f943f13087e6663c98aeb39162167d00c48e
TriveshWasnik <92360264+TriveshWasnik@users.noreply.github.com>
2024-09-29 01:25:41 +0530

    Update

--- controllers/wishlist/listWishlist.js
+++ controllers/wishlist/listWishlist.js
@@ -1,17 +1,13 @@
-import { Wishlist } from "../../models/wishlist.model.js";
+import { User } from "../../models/user.model.js";
 
 // get all Wishlist Products
 export const listWishProduct = async function (req, res) {
   try {
-    const products = await Wishlist.find({})
-      .populate("user", "name email phoneNumber")
-      .populate(
-        "product",
-        "name productImage1 description weight weightType mrpPrice"
-      );
+    const userId = req.user.id;
+    const user = await User.findById(userId).populate("favourites").exec();
+    
     return res.status(200).json({
-      data: products,
-      total: products.length,
+     user.favourites,
       success: true,
     });
   } catch (error) {

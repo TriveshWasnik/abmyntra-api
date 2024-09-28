ef3638df8811b2c03bc424df7a59b801a34c0cf3
TriveshWasnik <92360264+TriveshWasnik@users.noreply.github.com>
2024-09-29 01:17:05 +0530

    Update

--- controllers/order/orderListUser.js
+++ controllers/order/orderListUser.js
@@ -1,43 +1,24 @@
-import { Cart } from "../../models/cart.model.js";
+
 import { Order } from "../../models/order.model.js";
 
 // Order list user
 export const orderListUser = async function (req, res) {
   try {
-    const userId = req.id;
+   
+   const { productId } = req.body;
+    const userJWT = req.user;
+    const user = await User.findById(userJWT.id);
 
-    const orderList = await Order.find({ userId });
-
-    if (!orderList || orderList.length === 0) {
-      return res
-        .status(400)
-        .json({ message: "No Order Found", success: false });
+    if (!user.favourites.includes(productId)) {
+      user.favourites.push(productId);
+      await user.save();
     }
 
-    const orderWithTotalItems = await Promise.all(
-      orderList.map(async (order) => {
-        // Fetch cart items for each order
-        const cartInfo = await Cart.find({ orderId: order.orderId });
-        // calculate total items
-        let totalItems = 0;
-        cartInfo.forEach((item) => {
-          totalItems = item.productQuantity;
-        });
-        return {
-          orderId: order.orderId,
-          userName: order.shippingName,
-          orderDate: order.orderDate,
-          orderStatus: order.orderStatus,
-          grandTotalAmount: order.grandTotalAmount,
-          totalItems: totalItems,
-        };
-      })
-    );
+    return res
+      .status(200)
+      .json({ message: "Product added to favorites successfully", user });
 
-    return res.status(200).json({
-      data: orderWithTotalItems,
-      success: true,
-    });
+   
   } catch (error) {
     console.log(error.message);
     res

22618de09cea184570b1dc65863ae285dbb5ec12
TriveshWasnik <92360264+TriveshWasnik@users.noreply.github.com>
2024-09-28 23:50:17 +0530

    Update

--- server.js
+++ server.js
@@ -16,7 +16,13 @@ const PORT = process.env.PORT || 5000;
 connectDB(process.env.MONGODB_URL);
 
 // Middlewares
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 //app.use(express.static("public"));
